31d60b64324b3c6de41129ac5979f3a2342c004f
gholtzap <gholtzap@asu.edu>
2023-07-08 00:39:04 -0400

    timing

---
+++ timing/timing.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import os
+import yfinance as yf
+import bt
+from datetime import datetime, timedelta
+
+directory = 'data/'
+initial_balance = 10000
+portfolio = {}
+
+
+def calc_2q(directory):
+    ticker_averages = {}
+
+    for ticker in os.listdir(directory):
+        if not os.path.isdir(os.path.join(directory, ticker)):
+            continue
+
+        for file in os.listdir(os.path.join(directory, ticker)):
+            if file.endswith('.csv'):
+                file_path = os.path.join(directory, ticker, file)
+                df = pd.read_csv(file_path)
+
+                df['date'] = pd.to_datetime(df['Date'])
+                df.set_index('date', inplace=True)
+
+                resampled_df = df.resample('6M').mean(numeric_only=True)
+
+                second_lowest_average = resampled_df['Close'].nsmallest(
+                    2).iloc[-1]
+
+                ticker_averages[ticker] = second_lowest_average
+
+    return ticker_averages
+
+
+averages = calc_2q(directory)
+
+
+def should_buy_stock(ticker_averages):
+    decisions = {}
+
+    for ticker, average in ticker_averages.items():
+        data = yf.Ticker(ticker)
+        history = data.history(period='1d')
+        current_price = history['Close'][0]
+
+        change_needed = average - current_price
+
+        if current_price < average:
+            decisions[ticker] = ('Buy', change_needed, current_price, average)
+        else:
+            decisions[ticker] = (
+                'Do not buy', change_needed, current_price, average)
+
+    return decisions
+
+
+buy_decisions = should_buy_stock(averages)
+
+sorted_decisions = sorted(buy_decisions.items(), key=lambda x: (
+    x[1][0] == 'Do not buy', abs(x[1][1])))
+
+for ticker, (decision, change_needed, current_price, average) in sorted_decisions:
+    print(
+        f'Ticker: {ticker}, Decision: {decision}, Change Needed: {change_needed}')
---
+++ timing/usage.md
@@ -0,0 +1,20 @@
+**TIMING**
+
+How to use:
+
+1. Delcare your desired tickers in ``tickers.json``
+2. Run ``gem_data.py`` to generate data for those tickers
+3. Run ``timing.py``
+
+The output will display which stocks you should or shouldn't buy from your list, ordered from best to worst.
+
+
+Purpose of each file:
+
+- gen_data.py: generates 4x 6 month intervals and 2 year time series data for each ticker
+- timing.py: evaluates stocks according to the 2Q formula and determines if it is a buy or not
+
+
+2Q formula:
+
+- takes the average of each of the four six-month intervals. The second lowest average is the 'bar'. If a stock is below the bar, you buy it.
