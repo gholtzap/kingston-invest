0facf1767867c0d517ba4895a87179a045ebe23b
gholtzap <gholtzap@asu.edu>
2023-07-19 21:13:23 -0400

    diwi update

--- strategies/formulas/diwi.py
+++ strategies/formulas/diwi.py
@@ -1,46 +1,69 @@
 import os
 import pandas as pd
-from generate_data import tickers
+import numpy as np
+import json
 
-print(f"\nRUNNING FORMULA_DIWI\n")
-
+base_dir = "../data"
 totals = {}
 sma = {}
 
-def formula(ticker, filename):
-
-    data = pd.read_csv(f'data/{ticker}/{filename}')
-
+def calculate_score(data):
     data['PctChange'] = data['Close'].pct_change()
-
     data['30DaySMA'] = data['Close'].rolling(window=30).mean()
-
     data['HardDip'] = data['Close'] < (data['30DaySMA'] * 0.8)
-
     hard_dips = data[data['HardDip']].shape[0]
-
-    if ticker not in totals:
-        totals[ticker] = [hard_dips]
-    else:
-        totals[ticker][0] += hard_dips
-
     return hard_dips
 
+for ticker in os.listdir(base_dir):
+    ticker_dir = os.path.join(base_dir, ticker)
 
-for ticker in tickers:
-    ticker_dir = f'data/{ticker}'
-    for filename in os.listdir(ticker_dir):
-        formula(ticker, filename)
-
+    if os.path.isdir(ticker_dir):
+        df = pd.read_csv(f"{ticker_dir}/{ticker}_5_year_data.csv")
+        hard_dips = calculate_score(df)
+        totals[ticker] = hard_dips
 
 num_tickers = 10
 top = sorted(totals.items(), key=lambda item: item[1], reverse=True)[:num_tickers]
-
 top_tickers = [item[0] for item in top]
 
-print('##################################')
-results = []
-for stock in top:
-    name = stock[0]
-    hard_dips = stock[1][0]
-    results.append([name, hard_dips])
+print(f"Selected stocks: {top_tickers}")
+
+budget = 10000
+funds = {stock: budget / len(top_tickers) for stock in top_tickers}
+
+dataframes = []
+for ticker in top_tickers:
+    ticker_dir = os.path.join(base_dir, ticker)
+    df = pd.read_csv(f"{ticker_dir}/{ticker}_5_year_data.csv")
+    df["Ticker"] = ticker
+    dataframes.append(df)
+
+all_data = pd.concat(dataframes)
+shares = {stock: funds[stock] / all_data[(all_data["Ticker"] == stock)].iloc[0]["Close"] for stock in top_tickers}
+
+portfolio_value = sum([shares[stock] * all_data[(all_data["Ticker"]
+                      == stock)].iloc[-1]["Close"] for stock in top_tickers])
+
+earned = portfolio_value/(budget/100.0)
+print(
+    f"Final value using this strategy: ${portfolio_value}, which is a %{earned:.2f} return")
+
+# Specify the file path where you want to save your JSON file
+json_file_path = 'output.json'
+
+# Read existing data
+with open(json_file_path, 'r') as file:
+    data = json.load(file)
+
+# Append new data to the 'Shares' key
+if "diwi" in data["Shares"]:
+    data["Shares"]["diwi"].update(shares)
+else:
+    data["Shares"]["diwi"] = shares
+
+# Append return to the top level of the dictionary
+data["diwi"] = earned
+
+# Write back to the file
+with open(json_file_path, 'w') as file:
+    json.dump(data, file)
--- strategies/formulas/test.bat
+++ strategies/formulas/test.bat
@@ -5,3 +5,4 @@ python ma.py
 python macd.py
 python nike.py
 python rk.py
+python iman.py
